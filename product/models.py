d1cab4190b5d99392912854c1917f8ec57cd9fe6
Igor Candido <igorcr39@gmail.com>
2023-01-17 19:50:38 -0300

    created products models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ProductClasses(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    classe = models.TextField()
+    quantity = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    classe = models.ForeignKey(
+        ProductClasses, related_name="product", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
