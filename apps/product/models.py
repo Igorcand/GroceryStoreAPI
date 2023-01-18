1c905b0d25a75a2ad48c8f17df9d6cb6b150eb4c
Igor Candido <igorcr39@gmail.com>
2023-01-18 06:37:11 -0300

    changed cascate to protected

--- apps/product/models.py
+++ apps/product/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     quantity = models.FloatField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(
-        Category, related_name="product", on_delete=models.CASCADE)
+        Category, related_name="product", on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.name
