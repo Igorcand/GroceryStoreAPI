a74fc9c6f7d88f8b92e512f7ab1245d83734450e
Igor Candido <igorcr39@gmail.com>
2023-01-18 09:36:44 -0300

    renamed app produtc to products

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.product import models 
+from apps.products import models 
 
 # O que é a serialização: é a conversão de objetos pyton para uma forma que facilite a integração com diferentes sistemas, o JSON. Então o serializer vai converter o model do python para json 
 class ProductsSerializer(serializers.ModelSerializer):
