ec3621ce1961c258fc3795d46a6369316fe54531
Igor Candido <igorcr39@gmail.com>
2023-01-18 13:42:15 -0300

    created reports table and registred on application

--- apps/reports/models.py
+++ apps/reports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Reports(models.Model):
+    product = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    quantity_itens = models.IntegerField()
+    stock = models.IntegerField()
+    sale = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.product} - {self.data}" 
--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'apps.products',
     'apps.sales',
+    'apps.reports',
+
 
 ]
 
