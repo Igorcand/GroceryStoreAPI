fa3b2f43faf0946adee3a44a4e1d2efaa71c2c61
Igor Candido <igorcr39@gmail.com>
2023-01-18 10:31:49 -0300

    changed product app name to products

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.product'
+    'apps.products',
+    'apps.sales',
+
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('api/', include('apps.product.urls'))
+    path('api/', include('apps.products.urls'))
     ]
