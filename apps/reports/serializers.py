453a416a2e6c3fae9157e14515794b0d3f3b1eeb
Igor Candido <igorcr39@gmail.com>
2023-01-18 14:21:39 -0300

    created serializers

---
+++ apps/reports/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from apps.reports import models
+
+class ReportsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = models.Reports 
+        fields = '__all__'
