fbbe1a7bb05208f558ca844c26c3f8690caa8ba8
Igor Candido <igorcr39@gmail.com>
2023-01-18 05:46:04 -0300

    renamed product class table to category

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductClasses
+from .models import Product, Category
 
 admin.site.register(Product)
-admin.site.register(ProductClasses)
+admin.site.register(Category)
--- apps/product/models.py
+++ apps/product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class ProductClasses(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -9,11 +9,10 @@ class ProductClasses(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    classe = models.TextField()
     quantity = models.FloatField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    classe = models.ForeignKey(
-        ProductClasses, related_name="product", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="product", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
